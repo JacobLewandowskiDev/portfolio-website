98e22da2e28afb5606d7cdac8de050e7a85ce4e4
Jacob <lewandowskiJakub9@gmail.com>
2024-03-24 23:32:38 -0400

    This commit is a WIP on the main home page

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,21 +1,27 @@
 <script>
-export default {};
+export default {
+
+};
 </script>
 
 <template>
   <div class="card">
-    <img src="" />
-    <p>Description</p>
-    <router-link to="{{ link }}" type="button">About</router-link>
+    <img class="card__img" src="" />
+    <p class="card__description">Description</p>
+    <router-link class="card__link" :to="{ Link }" type="button">About</router-link>
   </div>
 </template>
 
 <style scoped>
 .card {
   position: relative;
-  width: 300px;
-  height: 350px;
-  margin: 25px 40px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  width: 12rem;
+  height: 15rem;
+  margin: 0 1rem;
   border-right: 1px solid;
   border-bottom: 1px solid;
   border-color: var(--card-border-color);
@@ -26,6 +32,15 @@ export default {};
   -webkit-backdrop-filter: blur(6.1px);
 }
 
-
+.card__link {
+  position: absolute;
+  color: var(--font-color);
+  text-decoration: none;
+  background-color: var(--card-link-bg-color);
+  /* border: 2px solid yellow; */
+  padding: .5rem 1rem;
+  border-radius: .5rem;
+  bottom: 20%;
+}
 
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,22 +1,20 @@
 <script>
 import Card from "../components/Card.vue";
-
+import Navigation from "../components/Navigation.vue";
 export default {
   components: {
     Card: Card,
+    Navigation: Navigation
   },
 };
 </script>
 
 <template>
-  <header class="contact">
-    <img class="contact__icon" src="../assets/envelope-solid-icon.svg">
-    <img class="contact__icon" src="../assets/github-icon.svg">
-    <img class="contact__icon" src="../assets/linkedin-icon.svg">
+  <header>
+    <Navigation/>
   </header>
   <div class="page-container">
     <h1 class="name">Jacob Lewandowski</h1>
-
     <div class="page-container__tiles">
       <Card class="card" />
       <Card class="card" />
@@ -27,23 +25,28 @@ export default {
 
 <style scoped>
 .name {
-  text-align: right;
-  border: 2px solid yellow;
-  height: 40vh;
+  display: flex;
+  align-items: center;
+  justify-content: right;
+  text-align: center;
+  line-height: 1;
+  margin-top: 10vh;
+  word-break: keep-all;
+  height: 30vh;
 }
 
 .page-container__tiles {
-  position: relative;
+  position: absolute;
   display: flex;
   justify-content: center;
+  align-items: center;
   flex-wrap: wrap;
-  height: 40vh;
+  height: 50vh;
   width: 100%;
   margin: 0 auto;
-  margin-top: 30px;
 }
 
 .card:nth-child(2) {
-  margin-top: -15px;
+  margin-top: -2rem;
 }
 </style>
